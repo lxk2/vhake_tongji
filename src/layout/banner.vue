4834fe28588f09de06f460a9f401dd5e07bce7b5
lxk2 <737697366@qq.com>
2019-12-13 22:42:28 +0800

    abc

--- src/layout/banner.vue
+++ src/layout/banner.vue
@@ -1,20 +1,26 @@
 <template>
     <div>
       <div class="banner">
-        <div class="main">
-          <p>
-            <img src="@/assets/icon/time.png" alt="">
-            <span>2019-11-25 09:09:00</span>
-            <span class="last">QQ同时在线人数</span>
-          </p>
-          <p>
-            <strong>303,071,376</strong>
-            <span>历史最高：332,164,438</span>
-          </p>
-          <p>
-            <span>所有数据均来自腾讯QQ</span>
-          </p>
+        <div class="box">
+          <div class="img">
+            <img src="@/assets/icon/homepage_logo.png" alt="">
+          </div>
+          <div class="main">
+            <p>
+                <img src="@/assets/icon/time.png" alt="">
+                <span>2019-11-25 09:09:00</span>
+                <span class="last">百度点击量</span>
+            </p>
+            <p>
+              <strong>303,071,376</strong>
+              <span>历史最高：332,164,438</span>
+            </p>
+            <p>
+              <span>所有数据均来自百度统计</span>
+            </p>
+          </div>
         </div>
+
       </div>
     </div>
 </template>
